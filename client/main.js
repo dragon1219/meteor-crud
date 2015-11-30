0f040c1e243052bcdbeecd3cef4e110c214409bb
Fucheng Long <dragon1219app@gmail.com>
2015-11-30 12:52:20 +0800

    ui

--- client/main.js
+++ client/main.js
@@ -28,4 +28,11 @@ Meteor.subscribe("photos");
       Session.set('counter', Session.get('counter') + 1);
     }
   });
-
+Template.ads.onRendered(function() {
+  $.getScript("//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", function() {
+    var ads, adsbygoogle;
+    ads = '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-5046136522435444" data-ad-slot="1218246011" data-ad-format="auto"></ins>';
+    $('.ads').html(ads);
+    return (adsbygoogle = window.adsbygoogle || []).push({});
+  });
+});
